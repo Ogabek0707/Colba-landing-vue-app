3c989163ee82e6a7e95b4bf9d5ffc2be79f79e6d
Og'abek Ibragimov <ogabekibragimov@Ogabeks-MacBook-Air.local>
2025-01-31 13:27:22 +0500

    Fixed build issues

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import veeValidatePlugins from './plugins/vee-validate'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+// import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import VueTheMask from "vue-the-mask"
 import 'element-plus/dist/index.css'
 import "@hennge/vue3-pagination/dist/vue3-pagination.css";
@@ -20,9 +20,9 @@ app.use(router)
 app.use(VueTheMask)
 app.use(veeValidatePlugins)
 .component('font-awesome-icon', FontAwesomeIcon)
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-       app.component(key, component)
-}
+// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+//        app.component(key, component)
+// }
 
 app.mount('#app')
 export default app;
