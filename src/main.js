9b7b26c1f5f7af916c4a2a43db38e69e6b098d32
Og'abek Ibragimov <ogabekibragimov@Ogabeks-MacBook-Air.local>
2025-01-31 13:10:33 +0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,29 @@
+import './style.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+import veeValidatePlugins from './plugins/vee-validate'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import VueTheMask from "vue-the-mask"
+import 'element-plus/dist/index.css'
+import "@hennge/vue3-pagination/dist/vue3-pagination.css";
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(VueTheMask)
+app.use(veeValidatePlugins)
+.component('font-awesome-icon', FontAwesomeIcon)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+       app.component(key, component)
+}
+
+app.mount('#app')
+export default app;
---
+++ src/stores/auth.js
@@ -0,0 +1,15 @@
+import { defineStore } from 'pinia'
+import authApi from '../api/auth/AuthApi'
+import Notification from '../plugins/Notification'
+import router from '@/router/index'
+
+
+export const useAuthStore = defineStore({
+  id: "auth",
+  state:()=>({
+    
+  }),
+  actions: {
+    
+  }
+})
